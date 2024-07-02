a739a4e719091bcf82d632e53e60dfad76293be8
Ivan Lee <ivanklee86@gmail.com>
2024-07-02 09:58:16 +0000

    Bootstrap devcontainer and tenv.

---
+++ .devcontainer/post_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+
+# Install OpenTofu version
+tenv tofu install
+
+# Set up GPG if not codespaces
+if [ "$CODESPACES" != "true" ]; then
+    echo "Running locally, configure gpg."
+
+    git config --global gpg.program gpg2
+    git config --global user.signingkey ivanklee86@gmail.com
+    git config --global commit.gpgsign true
+else
+    echo "Running in codespaces."
+fi
