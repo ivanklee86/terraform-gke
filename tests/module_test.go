e3594c2956d1b2635e6d70b45bc46f66cb850e3a
Ivan Lee <ivanklee86@gmail.com>
2024-07-03 06:05:48 -0400

    Bootstrap repository (#2)
    * Add terraform-docs tool.
    
    * Set up Terraform files.
    
    * Bootstrap tests.
    
    * Add taskfile.
    
    * Hit push with a hammer.

---
+++ tests/module_test.go
@@ -0,0 +1,22 @@
+package tests
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformHelloWorldExample(t *testing.T) {
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "./example",
+	})
+
+	// Always clean up terraform
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	output := terraform.Output(t, terraformOptions, "hello_world")
+	assert.Equal(t, "Hello, World!", output)
+}
